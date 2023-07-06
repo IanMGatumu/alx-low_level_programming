8d3c9b0849fd7ed1ab7440c912697d83be5a02a6
IanMGatumu <mweshiols@gmail.com>
2023-07-06 14:32:16 -0700

    correction of syntax error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,13 +4,13 @@
  * print_binary - function prints from decimal to the binary equivalent
  * @n: passed intager value
  * return void
- */
+*/
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
+	int a, count = 0;
 	unsigned long int current;
 
-	for (i = 63; i >= 0; a--)
+	for (a = 63; a >= 0; a--)
 	{
 		current = n >> a;
 
